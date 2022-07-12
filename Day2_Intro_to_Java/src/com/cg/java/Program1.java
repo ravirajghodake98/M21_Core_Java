4498a50b8ea4a4a16748ca225d9cc625c3ac3c52
ravirajghodake98 <ravirajghodake98@gmail.com>
2022-07-12 13:27:47 +0530

    First commit

---
+++ Day2_Intro_to_Java/src/com/cg/java/Program1.java
@@ -0,0 +1,15 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+//		int a = 15;
+		Scanner s = new Scanner(System.in);
+		double a = s.nextDouble();
+		System.out.println("The value of a is: " +a);
+//		System.out.println("how you doing");
+	}
+
+}
---
+++ Day2_Intro_to_Java/src/com/cg/java/Program2.java
@@ -0,0 +1,14 @@
+package com.cg.java;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		System.out.println("The name is: " +str);
+		s.close();
+	}
+
+}
